0fca83c9414c6d19e56914a895b5a1f1eaf82e1c
CristianPoma <pomacristian421@gmail.com>
2020-02-26 14:29:08 -0500

    26-02-2020

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/chat', {
+mongoose.connect('mongodb+srv://Black16:alexanderpoma16@cluster0-rwdl8.mongodb.net/test?retryWrites=true&w=majority', {
         useNewUrlParser: true
     })
     .then(db => console.log('CONECTADO A LA BDD'))
